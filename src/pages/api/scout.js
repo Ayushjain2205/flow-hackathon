a5842efbc8a2ec23afcd9d76ff5e1b38956dd038
Ayush Jain <ayushjain2205@gmail.com>
2023-07-10 16:26:33 +0530

    Add placeholder scouting API

--- src/pages/api/scout.js
+++ src/pages/api/scout.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer")
-const { NextApiRequest, NextApiResponse } = require("next")
 
 export default async function handler(req, res) {
   try {
@@ -7,36 +6,32 @@ export default async function handler(req, res) {
     const page = await browser.newPage()
 
     await page.goto(
-      "https://nft.flowverse.co/marketplace/FlowverseTreasures?isMulti=true&currency=FLOW&maxPrice=10"
+      "https://nft.flowverse.co/marketplace/FlowverseTreasures?isMulti=true&currency=FLOW&maxPrice=10",
+      { waitUntil: "networkidle2" }
     )
 
-    const items = await page.evaluate(() => {
-      const parentDiv = document.querySelector(
-        "#__next > div.h-full.relative.bg-black > div > div.mx-auto.px-[16px].lg:px-[32px].2xl:px-[64px].bg-black.py-0.pb-5.sm:py-8 > div.p-0.pt-16.sm:pt-10.text-gray-100 > div.py-3 > div.flex.flex-col.md:flex-row.md:space-x-4 > div.w-full.h-auto > div.p-4.pl-0.pr-0.grid.grid-cols-1.xs:grid-cols-2.lg:grid-cols-3.xl:grid-cols-4.2xl:grid-cols-5.4xl:grid-cols-6.gap-6"
-      )
-      const itemDivs = Array.from(parentDiv.children)
+    await page.waitForSelector(".w-full.h-auto > div:nth-child(2) > div")
 
-      console.log(itemDivs) // Display itemDivs in the terminal
-
-      return itemDivs.map((itemDiv) => {
-        const nameElement = itemDiv.querySelector("h5")
-        const linkElement = itemDiv.querySelector(".name-list-2.blue")
-
-        const name = nameElement?.textContent?.trim() || ""
-        const link = linkElement?.getAttribute("href") || ""
-
+    const parentElement = await page.$(".w-full.h-auto > div:nth-child(2)")
+    console.log(parentElement)
+    const childDivs = await page.$$eval(".w-full.h-auto > div:nth-child(2) > div", (divs) => {
+      return divs.map((div) => {
+        // const image = div.querySelector("img.aspect-square").getAttribute("src")
+        const name = div.querySelector("h5").textContent
+        //const price = div.querySelector(".flex.font-bold.text-xs.sm:text-sm").textContent
         return {
+          // image,
           name,
-          link,
+          //price
         }
       })
     })
 
     await browser.close()
 
-    res.status(200).json(items)
+    res.status(200).json({ divs: childDivs })
   } catch (error) {
-    console.error("Error scraping items:", error)
-    res.status(500).json({ error: "An error occurred while scraping items" })
+    console.error("Error scraping element:", error)
+    res.status(500).json({ error: "An error occurred while scraping element" })
   }
 }
