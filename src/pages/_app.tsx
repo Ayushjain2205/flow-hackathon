52e2c1d0ad085860874c1fa8d780c9538b002bcf
Ayush Jain <ayushjain2205@gmail.com>
2023-07-10 17:10:50 +0530

    Configure DaisyUI setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps as NextAppProps } from "next/app"
 import { AuthProvider } from "../components/AuthProvider"
 import { ComponentWithAuth } from "../components/ComponentWithAuth"
-import theme from "../lib/chakra-theme"
 import Head from "next/head"
 import { NiftoryClientProvider } from "graphql/niftoryClientProvider"
 import { NiftoryWalletInitializer } from "@components/NiftoryWalletInitializer"
@@ -23,7 +22,7 @@ const App = ({ Component, pageProps: { session, auth, ...pageProps } }: AppProps
       <SessionProvider session={session} refetchInterval={60 * 60}>
         <AuthProvider requireAuth={Component.requireAuth}>
           <NiftoryClientProvider>
-            <ChakraProvider theme={theme}>
+            <ChakraProvider>
               <NiftoryWalletInitializer />
               <Component {...pageProps} />
             </ChakraProvider>
