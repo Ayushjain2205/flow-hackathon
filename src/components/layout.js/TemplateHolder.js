7655832f23bb3805349c1459bec5dc2cf19f653a
Ayush Jain <ayushjain2205@gmail.com>
2023-07-12 20:14:03 +0530

    Update template holder for width to fit content

--- src/components/layout.js/TemplateHolder.js
+++ src/components/layout.js/TemplateHolder.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const TemplateHolder = ({ title, children }) => {
   return (
-    <div className="rounded-[18px] bg-white min-h-[200px]">
+    <div className="rounded-[18px] bg-white min-h-[200px] w-fit">
       <div className="h-[60px] flex flex-row justify-between p-[16px] bg-[#f8f8f8] rounded-t-[18px] text-[20px]">
         <p>{title}</p>
         <img className="h-[24px] rotate-45 cursor-pointer" src="/plus.svg" alt="" />
       </div>
-      <div className="flex flex-col p-[16px] pt-[24px]">{children}</div>
+      <div className="flex flex-col p-[16px] pt-[24px] items-center">{children}</div>
     </div>
   )
 }
