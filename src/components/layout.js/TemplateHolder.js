3107ec6fd94e402a01d1c6fc616e47b4dc2fdc09
Ayush Jain <ayushjain2205@gmail.com>
2023-07-15 22:54:18 +0530

    Add template holder box shadow

--- src/components/layout.js/TemplateHolder.js
+++ src/components/layout.js/TemplateHolder.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const TemplateHolder = ({ title, children }) => {
   return (
-    <div className="rounded-[18px] bg-white min-h-[200px] w-fit">
+    <div className="rounded-[18px] bg-white min-h-[200px] w-fit shadow-NFT">
       <div className="h-[60px] flex flex-row justify-between p-[16px] bg-[#f8f8f8] rounded-t-[18px] text-[20px]">
         <p className="text-black">{title}</p>
         <img className="h-[24px] rotate-45 cursor-pointer" src="/plus.svg" alt="" />
