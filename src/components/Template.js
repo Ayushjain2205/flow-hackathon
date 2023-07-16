a8654983c9aced4059271523ed8e61ba7a511779
Ayush Jain <ayushjain2205@gmail.com>
2023-07-16 16:43:40 +0530

    Upadate api route back to prod

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,51 +1,53 @@
-import React from "react";
-import SendMoney from "./templates/SendMoney";
-import Reminder from "./templates/Reminder";
-import BuyNFT from "./templates/BuyNFT";
-import Task from "./templates/Task";
-import SmartContract from "./templates/SmartContract";
-import Debug from "./templates/Debug";
-import WalletHealth from "./templates/WalletHealth";
-import Drop from "./templates/Drop";
-import None from "./templates/None";
+import React from "react"
+import SendMoney from "./templates/SendMoney"
+import Reminder from "./templates/Reminder"
+import BuyNFT from "./templates/BuyNFT"
+import Task from "./templates/Task"
+import SmartContract from "./templates/SmartContract"
+import Debug from "./templates/Debug"
+import WalletHealth from "./templates/WalletHealth"
+import Drop from "./templates/Drop"
+import None from "./templates/None"
 
 const Template = ({ template }) => {
-  let component;
+  let component
 
   switch (template) {
     case "SendMoney":
-      component = <SendMoney />;
-      break;
+      component = <SendMoney />
+      break
     case "Reminder":
-      component = <Reminder />;
-      break;
-    case "Buy":
-      component = <BuyNFT />;
-      break;
+      component = <Reminder />
+      break
+    case "buyNFT":
+      component = <BuyNFT />
+      break
     case "Task":
-      component = <Task />;
-      break;
+      component = <Task />
+      break
     case "SmartContract":
-      component = <SmartContract />;
-      break;
+      component = <SmartContract />
+      break
     case "Debug":
-      component = <Debug />;
-      break;
+      component = <Debug />
+      break
     case "WalletHealth":
-      component = <WalletHealth />;
-      break;
+      component = <WalletHealth />
+      break
     case "Drop":
-      component = <Drop />;
-      break;
+      component = <Drop />
+      break
     case "News":
-      component = <None text="Flow Price Prediction as $300 Million Pushes FLOW Up 35% – What's Going On?" />;
-      break;
+      component = (
+        <None text="Flow Price Prediction as $300 Million Pushes FLOW Up 35% – What's Going On?" />
+      )
+      break
     default:
-      component = <None text={template} />;
-      break;
+      component = <None text={template} />
+      break
   }
 
-  return component;
-};
+  return component
+}
 
-export default Template;
+export default Template
