2215a46764baaf71b93e16cbdc07fb48867f5b96
Ayush Jain <ayushjain2205@gmail.com>
2023-07-16 16:26:32 +0530

    Fix sample

--- src/pages/api/sample.js
+++ src/pages/api/sample.js
@@ -1,9 +1,4 @@
 import { NextRequest, NextResponse } from "next/server"
-import Replicate from "replicate"
-
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_TOKEN || "",
-})
 
 const KEY = process.env.OPENAI_API_KEY
 const base_uri = "https://api.openai.com/v1/chat/completions"
@@ -57,54 +52,13 @@ export async function POST(req, res) {
     if (function_call) {
       const args = JSON.parse(function_call.arguments)
       if (function_call.name === "createVideo") {
-        const output = await replicate.run(
-          "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
-          {
-            input: {
-              ...args,
-            },
-          }
-        )
-        return NextResponse.json({
-          data: output,
-          type: "video",
-        })
       }
       if (function_call.name === "createMusic") {
-        const output = await replicate.run(
-          "joehoover/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
-          {
-            input: {
-              model_version: "melody",
-              ...args,
-            },
-          }
-        )
-        return NextResponse.json({
-          data: output,
-          type: "audio",
-        })
       }
       if (function_call.name === "createImage") {
-        const output = await replicate.run(
-          "ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
-          {
-            input: {
-              ...args,
-            },
-          }
-        )
-        return NextResponse.json({
-          data: output,
-          type: "image",
-        })
       }
     } else {
       console.log("choice: ", choice)
-      return NextResponse.json({
-        data: choice.message.content,
-        type: "text",
-      })
     }
   } catch (err) {
     console.log("error: ", err)
--- src/pages/index.js
+++ src/pages/index.js
@@ -39,42 +39,6 @@ const HomePage = () => {
         </button>
       </div>
     </div>
-    // <AppLayout>
-    //   <Center py={{ base: "1rem" }} flexDir="column" position="relative">
-    //     <Box px="1rem">
-    //       <VStack>
-    //         {!session && (
-    //           <Box pt="200">
-    //             <Text p="5" textAlign="center" fontWeight="semibold" fontSize="xl">
-    //               Login to get started!
-    //             </Text>
-    //             <Button
-    //               p="8"
-    //               isLoading={isLoading}
-    //               onClick={() => signIn()}
-    //               colorScheme="red"
-    //               leftIcon={<FaGoogle />}
-    //             >
-    //               Sign in with Google
-    //             </Button>
-    //           </Box>
-    //         )}
-    //         {session && (
-    //           <VStack>
-    //             <WalletDetails
-    //               isLoading={fetching}
-    //               walletAddress={wallet?.address}
-    //               walletItems={wallet?.nfts?.length}
-    //               walletStatus={wallet?.state?.toString()}
-    //               walletOwnerEmail={wallet?.appUser?.email}
-    //             />
-    //             <Logout />
-    //           </VStack>
-    //         )}
-    //       </VStack>
-    //     </Box>
-    //   </Center>
-    // </AppLayout>
   )
 }
 
